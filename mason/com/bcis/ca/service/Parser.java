773f7b763522b3b682e5fc9fb16b805aafe16e23
Nawaz Gayoom <nawazg07@gmail.om>
2014-09-18 00:55:49 +1200

    draw.js updated

--- mason/com/bcis/ca/service/Parser.java
+++ mason/com/bcis/ca/service/Parser.java
@@ -3,7 +3,7 @@ package com.bcis.ca.service;
 import javax.ejb.Stateless;
 
 /**
- * Converts Simulation information into a presentable format to passed onto the
+ * Converts Simulation information into a presentable format passed onto the
  * presentation layer.
  * @author Nawaz Gayoom
  * @version 0.1 - 11/09/2014: created.
--- web/resources/js/Draw.js
+++ web/resources/js/Draw.js
@@ -6,4 +6,35 @@ file.
 @version 0.1 - 11/09/2014: Created.
  */
 
-
+$(document).ready(function () {
+    
+    var canvas = document.getElementById("simArea");
+    var ctx = canvas.getContext("2d");
+    
+    var posX = 0;
+    var posY = 0;
+    var cellWidth = 20;
+    var cellHeight = 20;
+    var columns = 20;
+    var rows = 20;
+    var width = cellWidth * columns;
+    var height = cellHeight * rows;
+    canvas.width = width;
+    canvas.height = height;
+    for(var i = 0; i < columns; i++){
+        for(var j = 0; j < rows; j++){
+            ctx.rect(posX,posY,cellWidth,cellHeight);
+            ctx.strokeStyle = "black";
+            ctx.stroke();
+            if(j === 3){
+                ctx.fillStyle = "black";
+                ctx.fill();
+            }
+            posX = posX + cellWidth;
+        }
+        posX = 0;
+        posY = posY + cellHeight;
+    }
+    
+    
+});
